fedf428170c9c0a2b1a9eb9653396cbc274e32ef
KUBO Atsuhiro <kubo@iteman.jp>
2006-04-18 05:30:15 +0000

    - Added.

---
+++ package.php
@@ -0,0 +1,116 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006, KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Stagehand_FSM
+ * @author     KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @copyright  2006 KUBO Atsuhiro <iteman2002@yahoo.co.jp>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://iteman.typepad.jp/stagehand/
+ * @see        Stagehand_TestRunner_PHPUnitTestRunner::runAll()
+ * @since      File available since Release 0.1.0
+ */
+
+require_once 'PEAR/PackageFileManager2.php';
+
+$version = '1.2.0';
+$notes = 'The first release of Stagehand_FSM.';
+
+$package = new PEAR_PackageFileManager2();
+$result = $package->setOptions(array('filelistgenerator' => 'svn',
+                                     'changelogoldtonew' => false,
+                                     'simpleoutput'      => true,
+                                     'baseinstalldir'    => '/',
+                                     'packagefile'       => 'package2.xml',
+                                     'packagedirectory'  => '.')
+                               );
+
+if (PEAR::isError($result)) {
+    print $result->getMessage();
+    exit();
+}
+
+$package->setPackage('Stagehand_FSM');
+$package->setPackageType('php');
+$package->setSummary('A Finite State Machine.');
+$package->setDescription('Stagehand_FSM provides a self configuring Finite State Machine.
+The following is a list of features of Stagehand_FSM.
+o Transition action
+o Entry and Exit state actions
+o Initial and Final pseudo states
+o Nested FSM
+o History Marker
+o Activity
+o User defined payload');
+$package->setChannel('pear.hatotech.org');
+$package->setLicense('BSD License (revised)',
+                     'http://www.opensource.org/licenses/bsd-license.php'
+                     );
+$package->setAPIVersion('1.2.0');
+$package->setAPIStability('stable');
+$package->setReleaseVersion($version);
+$package->setReleaseStability('stable');
+$package->setNotes($notes);
+$package->setPhpDep('4.2.0');
+$package->setPearinstallerDep('1.4.3');
+$package->addMaintainer('lead', 'kubo', 'KUBO Atsuhiro', 'iteman2002@yahoo.co.jp');
+$package->addMaintainer('developer', 'miya', 'MIYAI Fumihiko', 'fumichz@yahoo.co.jp');
+$package->addIgnore(array('package.php', 'package.xml', 'package2.xml'));
+$package->addGlobalReplacement('package-info', '@package_version@', 'version');
+$package->generateContents();
+$package1 = &$package->exportCompatiblePackageFile1();
+
+if (array_key_exists(1, $_SERVER['argv'])
+    && $_SERVER['argv'][1] == 'make'
+    ) {
+    $result = $package->writePackageFile();
+    $result = $package1->writePackageFile();
+} else {
+    $result = $package->debugPackageFile();
+    $result = $package1->debugPackageFile();
+}
+
+if (PEAR::isError($result)) {
+    print $result->getMessage();
+}
+
+exit();
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
