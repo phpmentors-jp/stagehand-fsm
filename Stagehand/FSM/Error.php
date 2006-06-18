53775ba4547e586e6bf6d11af9d953f9bc672653
KUBO Atsuhiro <kubo@iteman.jp>
2006-06-18 05:29:29 +0000

    - Changed the method name from raiseError() to push() and added an   argument to push() method for error levels. - Added pushCallback(), popCallback(), hasErrors(), pop(), and   clearErrors() methods to avoid using PEAR_ErrorStack class directly. - Removed getErrorStack() and isError() methods. - Updated some method descriptions.

--- Stagehand/FSM/Error.php
+++ Stagehand/FSM/Error.php
@@ -83,65 +83,30 @@ class Stagehand_FSM_Error
      */
 
     // }}}
-    // {{{ getErrorStack()
+    // {{{ push()
 
     /**
-     * Returns a single error stack for the package.
-     *
-     * @param string $package
-     * @return PEAR_ErrorStack
-     */
-    function &getErrorStack($package = 'Stagehand_FSM')
-    {
-        $stack = &PEAR_ErrorStack::singleton(strtolower($package));
-        return $stack;
-    }
-
-    // }}}
-    // {{{ raiseError()
-
-    /**
-     * Returns a PEAR_ErrorStack object for the package.
+     * Adds an error to the stack for the package. This method is a wrapper
+     * for PEAR_ErrorStack::staticPush() method.
      *
      * @param integer $code
      * @param string  $message
+     * @param string  $level
      * @param array   $params
-     * @param string  $package
+     * @param array   $repackage
      * @param array   $backtrace
-     * @return PEAR_ErrorStack
+     * @see PEAR_ErrorStack::staticPush()
+     * @since Method available since Release 1.4.0
      */
-    function raiseError($code,
-                        $message = false,
-                        $params = array(),
-                        $package = 'Stagehand_FSM',
-                        $backtrace = false
-                        )
+    function push($code, $message = false, $level = 'exception',
+                  $params = array(), $repackage = false, $backtrace = false
+                  )
     {
         if (!$backtrace) {
             $backtrace = debug_backtrace();
         }
 
-        $stack = &Stagehand_FSM_Error::getErrorStack($package);
-        $stack->push($code, 'error', $params, $message, false, $backtrace);
-        return $stack;
-    }
-
-    // }}}
-    // {{{ isError()
-
-    /**
-     * Returns whether the value is a PEAR_ErrorStack object.
-     *
-     * @param mixed $error
-     * @return boolean
-     */
-    function isError($error)
-    {
-        if (is_object($error) && is_a($error, 'PEAR_ErrorStack')) {
-            return true;
-        }
-
-        return false;
+        PEAR_ErrorStack::staticPush('Stagehand_FSM', $code, $level, $params, $message, $repackage, $backtrace);
     }
 
     // }}}
@@ -149,9 +114,11 @@ class Stagehand_FSM_Error
 
     /**
      * Pushes a callback. This method is a wrapper for
-     * PEAR_ErrorStack::staticPushCallback method.
+     * PEAR_ErrorStack::staticPushCallback() method.
      *
      * @param callback $callback
+     * @see PEAR_ErrorStack::staticPushCallback()
+     * @since Method available since Release 1.4.0
      */
     function pushCallback($callback)
     {
@@ -163,9 +130,11 @@ class Stagehand_FSM_Error
 
     /**
      * Pops a callback. This method is a wrapper for
-     * PEAR_ErrorStack::staticPopCallback method.
+     * PEAR_ErrorStack::staticPopCallback() method.
      *
      * @return callback
+     * @see PEAR_ErrorStack::staticPopCallback()
+     * @since Method available since Release 1.4.0
      */
     function popCallback()
     {
@@ -177,15 +146,42 @@ class Stagehand_FSM_Error
 
     /**
      * Returns whether the stack has errors or not. This method is a wrapper
-     * for PEAR_ErrorStack::staticHasErrors method.
+     * for PEAR_ErrorStack::staticHasErrors() method.
      *
-     * @param string $package
      * @param string $level
-     * @return callback
+     * @return boolean
+     * @see PEAR_ErrorStack::staticHasErrors()
+     * @since Method available since Release 1.4.0
      */
-    function hasErrors($package = false, $level = false)
+    function hasErrors($level = false)
     {
-        return PEAR_ErrorStack::staticHasErrors($package, $level);
+        return PEAR_ErrorStack::staticHasErrors('Stagehand_FSM', $level);
+    }
+
+    /**
+     * Pops an error off of the error stack for the package. This method is a
+     * wrapper for PEAR_ErrorStack::pop() method.
+     *
+     * @return array
+     * @see PEAR_ErrorStack::pop()
+     * @since Method available since Release 1.4.0
+     */
+    function pop()
+    {
+        $stack = &PEAR_ErrorStack::singleton('Stagehand_FSM');
+        return $stack->pop();
+    }
+
+    /**
+     * Clears the error stack for the package.
+     *
+     * @see PEAR_ErrorStack::getErrors()
+     * @since Method available since Release 1.4.0
+     */
+    function clearErrors()
+    {
+        $stack = &PEAR_ErrorStack::singleton('Stagehand_FSM');
+        $stack->getErrors(true);
     }
 
     /**#@+
