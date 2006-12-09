0d7a6a23e2befd9b474688c5cab1ee9259ec2caa
KUBO Atsuhiro <kubo@iteman.jp>
2006-12-09 11:38:10 +0000

    - Moved.

---
+++ tests/Stagehand/FSMTestCase/GateKeeper.php
@@ -0,0 +1,146 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * PHP versions 4 and 5
+ *
+ * Copyright (c) 2006 KUBO Atsuhiro <iteman@users.sourceforge.net>,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package    Stagehand_FSM
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    SVN: $Id$
+ * @link       http://piece-framework.com/
+ * @see        Stagehand_FSM
+ * @since      File available since Release 0.1.0
+ */
+
+// {{{ GateKeeper
+
+/**
+ * A gate keeper for Unit Tests.
+ *
+ * @package    Stagehand_FSM
+ * @author     KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @copyright  2006 KUBO Atsuhiro <iteman@users.sourceforge.net>
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License (revised)
+ * @version    Release: @package_version@
+ * @link       http://piece-framework.com/
+ * @see        Stagehand_FSM
+ * @since      Class available since Release 0.1.0
+ */
+class GateKeeper
+{
+
+    // {{{ properties
+
+    /**#@+
+     * @access public
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    /**#@+
+     * @access public
+     */
+
+    var $lockCalled = false;
+    var $unlockCalled = false;
+    var $alarmCalled = false;
+    var $thankCalled = false;
+    var $helloCalled = false;
+    var $helloLockedCalled = false;
+
+    function lock()
+    {
+        $this->lockCalled = true;
+    }
+
+    function unlock()
+    {
+        $this->unlockCalled = true;
+    }
+
+    function alarm()
+    {
+        $this->alarmCalled = true;
+    }
+
+    function thank()
+    {
+        $this->thankCalled = true;
+    }
+
+    function hello()
+    {
+        $this->helloCalled = true;
+    }
+
+    function helloLocked()
+    {
+        $this->helloLockedCalled = true;
+    }
+
+    function reset()
+    {
+        $this->lockCalled = false;
+        $this->unlockCalled = false;
+        $this->alarmCalled = false;
+        $this->thankCalled = false;
+        $this->helloCalled = false;
+        $this->helloLockedCalled = false;
+    }
+
+    /**#@-*/
+
+    /**#@+
+     * @access private
+     */
+
+    /**#@-*/
+
+    // }}}
+}
+
+// }}}
+
+/*
+ * Local Variables:
+ * mode: php
+ * coding: iso-8859-1
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * indent-tabs-mode: nil
+ * End:
+ */
+?>
