360304f561a3cacaed0c322acd6448650e61bb6a
KUBO Atsuhiro <kubo@iteman.jp>
2012-06-10 01:37:34 +0900

    Added the setName() method to the FSMBuilder class. (Issue #279)

--- src/Stagehand/FSM/FSMBuilder.php
+++ src/Stagehand/FSM/FSMBuilder.php
@@ -2,7 +2,7 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4: */
 
 /**
- * Copyright (c) 2011 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2011-2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,7 +27,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_FSM
- * @copyright  2011 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2012 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      File available since Release 2.0.0
@@ -37,7 +37,7 @@ namespace Stagehand\FSM;
 
 /**
  * @package    Stagehand_FSM
- * @copyright  2011 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2012 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      Class available since Release 2.0.0
@@ -183,6 +183,16 @@ class FSMBuilder
     public function setPayload(&$payload)
     {
         $this->fsm->setPayload($payload);
+    }
+
+    /**
+     * Sets the name of the FSM.
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->fsm->setName($name);
     }
 }
 
