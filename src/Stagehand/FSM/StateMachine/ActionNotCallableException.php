a65947fd49197b733f9536e0d32088dbdbfdaa5b
KUBO Atsuhiro <kubo@iteman.jp>
2013-07-24 19:55:05 +0900

    changed the base class of the ActionNotCallableException class to LogicException

--- src/Stagehand/FSM/StateMachine/ActionNotCallableException.php
+++ src/Stagehand/FSM/StateMachine/ActionNotCallableException.php
@@ -44,7 +44,7 @@ namespace Stagehand\FSM\StateMachine;
  * @version    Release: @package_version@
  * @since      Class available since Release 2.0.0
  */
-class ActionNotCallableException extends \InvalidArgumentException
+class ActionNotCallableException extends \LogicException
 {
 }
 
