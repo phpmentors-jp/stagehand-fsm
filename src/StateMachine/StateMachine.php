c95b5d44d7b5f2e5c3f013479a8af3fa8fc2ed3e
KUBO Atsuhiro <kubo@iteman.jp>
2014-08-29 13:33:45 +0900

    change the comment blocks of all PHP files to be simplified

--- src/StateMachine/StateMachine.php
+++ src/StateMachine/StateMachine.php
@@ -1,42 +1,13 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-
-/**
- * PHP version 5.3
- *
+/*
  * Copyright (c) 2006-2008, 2011-2014 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * This file is part of Stagehand_FSM.
  *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * @package    Stagehand_FSM
- * @copyright  2006-2008, 2011-2014 KUBO Atsuhiro <kubo@iteman.jp>
- * @license    http://opensource.org/licenses/BSD-2-Clause  The BSD 2-Clause License
- * @version    Release: @package_version@
- * @link       http://en.wikipedia.org/wiki/Finite_state_machine
- * @link       http://www.sparxsystems.com/resources/uml2_tutorial/uml2_statediagram.html
- * @link       http://pear.php.net/package/FSM
- * @link       http://www.generation5.org/content/2003/FSM_Tutorial.asp
- * @since      File available since Release 0.1.0
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
  */
 
 namespace Stagehand\FSM\StateMachine;
@@ -48,15 +19,11 @@ use Stagehand\FSM\Event\TransitionEventInterface;
 use Stagehand\FSM\State\StateInterface;
 
 /**
- * @package    Stagehand_FSM
- * @copyright  2006-2008, 2011-2014 KUBO Atsuhiro <kubo@iteman.jp>
- * @license    http://opensource.org/licenses/BSD-2-Clause  The BSD 2-Clause License
- * @version    Release: @package_version@
- * @link       http://en.wikipedia.org/wiki/Finite_state_machine
- * @link       http://www.sparxsystems.com/resources/uml2_tutorial/uml2_statediagram.html
- * @link       http://pear.php.net/package/FSM
- * @link       http://www.generation5.org/content/2003/FSM_Tutorial.asp
- * @since      Class available since Release 0.1.0
+ * @link  http://en.wikipedia.org/wiki/Finite_state_machine
+ * @link  http://www.sparxsystems.com/resources/uml2_tutorial/uml2_statediagram.html
+ * @link  http://pear.php.net/package/FSM
+ * @link  http://www.generation5.org/content/2003/FSM_Tutorial.asp
+ * @since Class available since Release 0.1.0
  */
 class StateMachine
 {
@@ -348,13 +315,3 @@ class StateMachine
         call_user_func($event->getAction(), $event, $this->getPayload(), $this);
     }
 }
-
-/*
- * Local Variables:
- * mode: php
- * coding: iso-8859-1
- * tab-width: 4
- * c-basic-offset: 4
- * indent-tabs-mode: nil
- * End:
- */
--- test/StateMachine/StateMachineTest.php
+++ test/StateMachine/StateMachineTest.php
@@ -1,38 +1,13 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-
-/**
- * PHP version 5.3
- *
+/*
  * Copyright (c) 2006-2008, 2011-2014 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * This file is part of Stagehand_FSM.
  *
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- * @package    Stagehand_FSM
- * @copyright  2006-2008, 2011-2014 KUBO Atsuhiro <kubo@iteman.jp>
- * @license    http://opensource.org/licenses/BSD-2-Clause  The BSD 2-Clause License
- * @version    Release: @package_version@
- * @since      File available since Release 0.1.0
+ * This program and the accompanying materials are made available under
+ * the terms of the BSD 2-Clause License which accompanies this
+ * distribution, and is available at http://opensource.org/licenses/BSD-2-Clause
  */
 
 namespace Stagehand\FSM\StateMachine;
@@ -43,11 +18,7 @@ use Stagehand\FSM\State\StateInterface;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
- * @package    Stagehand_FSM
- * @copyright  2006-2008, 2011-2014 KUBO Atsuhiro <kubo@iteman.jp>
- * @license    http://opensource.org/licenses/BSD-2-Clause  The BSD 2-Clause License
- * @version    Release: @package_version@
- * @since      Class available since Release 0.1.0
+ * @since Class available since Release 0.1.0
  */
 class StateMachineTest extends \PHPUnit_Framework_TestCase
 {
@@ -488,13 +459,3 @@ class StateMachineTest extends \PHPUnit_Framework_TestCase
         $this->fail('An expected exception has not been raised.');
     }
 }
-
-/*
- * Local Variables:
- * mode: php
- * coding: iso-8859-1
- * tab-width: 4
- * c-basic-offset: 4
- * indent-tabs-mode: nil
- * End:
- */
